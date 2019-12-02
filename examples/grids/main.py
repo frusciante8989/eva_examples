ec7b5f7d335700aa6e080237c42f82d0b14e6b5b
charlesworth <ch.c@automata.tech>
2019-12-02 16:07:50 +0000

    added Eva to the example

--- examples/grids/main.py
+++ examples/grids/main.py
@@ -1,4 +1,7 @@
 from typing import List, Tuple, NamedTuple
+import time
+
+from evasdk import Eva
 
 class XYPoint(NamedTuple):
     x: int
@@ -30,8 +33,10 @@ class Grid2D:
                 y = y_start - (row_step * row)
                 self.__positions.append(XYPoint(x = x, y = y))
 
+
     def __iter__(self):
         return self
+
 
     def __next__(self) -> XYPoint:
         if self.__current_position > len(self.__positions) - 1:
@@ -49,5 +54,31 @@ grid_corners: Grid_corners = [
 ]
 my_test_grid = Grid2D(grid_corners, rows = 4, columns = 4)
 
-for grid_position in my_test_grid:
-    print('x={:f}, y={:f}'.format(grid_position.x, grid_position.y))
+host_ip = input("Please enter a Eva IP: ")
+token = input("Please enter a valid Eva token: ")
+eva = Eva(host_ip, token)
+
+pose_guess = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+pose_home = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+end_effector_orientation = {'w': 0.0, 'x': 0.0, 'y': 0.0, 'z': 0.0}
+grid_z_position = 2
+
+with eva.lock():
+    eva.control_wait_for_ready()
+    eva.control_go_to(pose_home)
+
+    for grid_position in my_test_grid:
+        print('Eva going to grid position x={:f}, y={:f}'.format(grid_position.x, grid_position.y))
+        target_position = {'x': grid_position.x, 'y': grid_position.y, 'z': grid_z_position}
+        position_joint_angles = eva.calc_inverse_kinematics(pose_guess, target_position, end_effector_orientation)
+        eva.control_go_to(position_joint_angles['ik']['joints'])
+
+        # simulating an action with a sleep, i.e. picking up something from a pallet
+        print('Eva performing action at grid waypoint')
+        time.sleep(1)
+
+        print('Eva moving to home position')
+        eva.control_go_to(pose_home)
+
+
+
